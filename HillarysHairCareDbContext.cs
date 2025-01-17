9e6cb68f0d593bd6f16b11ca8a7d15af6783cbb9
Matthew Clonch <matthewrclonch@gmail.com>
2025-01-17 10:05:49 -0600

    initial commit

---
+++ HillarysHairCareDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using HillarysHairCare.Models;
+
+public class HillarysHairCareDbContext : DbContext
+{
+
+    public DbSet<Stylist> Stylists { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Service> Services { get; set; }
+    public DbSet<Appointment> Appointments { get; set; }
+    public DbSet<AppointmentService> AppointmentServices { get; set; }
+
+    public HillarysHairCareDbContext(DbContextOptions<HillarysHairCareDbContext> context) : base(context)
+    {
+
+    }
+}
